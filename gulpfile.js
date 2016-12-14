a7185ef5de7771bec67f85c27934fd22722b2b12
tbonFukitama <tbonfukitama110@gmail.com>
2016-12-15 04:25:46 +0900

    create gulp environment

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./",
+            index  : "index.html"
+        }
+    });
+});
+ 
+gulp.task("sass", function() {
+    gulp.src("./source/sass/**/*scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("js", function() {
+    gulp.src(["./source/js/**/*.js"])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("./js"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("reload", function() {
+    browser.reload();
+});
+
+gulp.task("default", ['server'], function() {
+    gulp.watch("./**/*.html", ["reload"]);
+    gulp.watch("./source/js/**/*.js",["js"]);
+    gulp.watch("./source/sass/**/*.scss",["sass"]);
+});
