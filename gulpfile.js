585e3c2bc49838917d5707cf0d3013304ded464e
tbonFukitama <tbonfukitama110@gmail.com>
2017-01-05 23:46:54 +0900

    ejsを導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require("gulp");
+var fs = require("fs");
+var ejs = require("gulp-ejs");
 var sass = require("gulp-sass");
 var uglify = require("gulp-uglify");
 var browser = require("browser-sync");
@@ -8,9 +10,21 @@ gulp.task("server", function() {
     browser({
         server: {
             baseDir: "./docs",
-            index  : "index.html"
+            startPath: '/ParmoSense/',
+            routes : {
+                '/ParmoSense': 'docs'
+            }
         }
     });
+});
+
+gulp.task("ejs", function() {
+    var json = JSON.parse(fs.readFileSync("./source/html/variables.json"));
+    gulp.src(["./source/html/**/*.ejs", "!./source/html/template/*.ejs"], { base: "./source/html" })
+        .pipe(plumber())
+        .pipe(ejs(json, {"ext": ".html"}))
+        .pipe(gulp.dest("./docs"))
+        .pipe(browser.reload({stream:true}));
 });
  
 gulp.task("sass", function() {
@@ -33,8 +47,8 @@ gulp.task("reload", function() {
     browser.reload();
 });
 
-gulp.task("default", ['server'], function() {
-    gulp.watch("./**/*.html", ["reload"]);
+gulp.task("default", ["server"], function() {
+    gulp.watch("./source/html/**/*.ejs", ["ejs"]);
     gulp.watch("./source/js/**/*.js",["js"]);
     gulp.watch("./source/sass/**/*.scss",["sass"]);
 });
